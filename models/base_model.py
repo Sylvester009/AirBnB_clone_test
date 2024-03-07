114f9ac95eb3637359c93e022fc64e3e32656363
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-07 12:09:06 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,26 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+
+        models.storage.new(self)
 
     def __str__(self):
         class_name = self.__class__.__name__
@@ -17,6 +30,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         inst_data = self.__dict__.copy()
@@ -39,3 +53,12 @@ if __name__ == "__main__":
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}"
               .format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
