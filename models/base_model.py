f80a571abf41efefa6ff16dd047753e26a896f73
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-07 13:47:24 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -63,16 +63,3 @@ if __name__ == "__main__":
 
     print("--")
     print(my_model is my_new_model)
-
-    all_objs = storage.all()
-    print("-- Reloaded objects --")
-    for obj_id in all_objs.keys():
-        obj = all_objs[obj_id]
-        print(obj)
-
-        print("-- Create a new object --")
-        my_model = BaseModel()
-        my_model.name = "My_First_Model"
-        my_model.my_number = 89
-        my_model.save()
-        print(my_model)
