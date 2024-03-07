41f30b15b5051d9e4b2651dbd70e0772aadb7e77
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-07 11:05:56 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,13 @@
 
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self):
         class_name = self.__class__.__name__
@@ -17,11 +16,24 @@ class BaseModel:
             class_name, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
-        data = self.__dict__.copy()
-        data['__class__'] = self.__class__.__name__
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
-        return data
+        inst_data = self.__dict__.copy()
+        inst_data["__class__"] = self.__class__.__name__
+        inst_data["created_at"] = self.created_at.isoformat()
+        inst_data["updated_at"] = self.updated_at.isoformat()
+        return inst_data
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
