b195820c91d904057f8ca35503d128828dce569b
Tinashe-123 <boysuncover1231@gmail.com>
2024-03-07 11:22:34 -0800

    Commit

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,33 +1,14 @@
 #!/usr/bin/python3
-
-import unittest
 from models.base_model import BaseModel
-from datetime import datetime
-import os
 
-class TestBaseModel(unittest.TestCase):
-    def test_init(self):
-        model = BaseModel()
-        self.assertIsInstance(model.id, str)
-        self.assertIsNotNone(model.created_at)
-        self.assertIsNotNone(model.updated_at)
-
-    def test_save(self):
-        model = BaseModel()
-        original_updated_at = model.updated_at
-        model.save()
-        self.assertNotEqual(model.updated_at, original_updated_at)
-
-    def test_to_dict(self):
-        model = BaseModel()
-        model_dict = model.to_dict()
-        self.assertIsInstance(model_dict, dict)
-        self.assertIn('__class__', model_dict)
-        self.assertEqual(model_dict['__class__'], 'BaseModel')
-        self.assertIn('id', model_dict)
-        self.assertIn('created_at', model_dict)
-        self.assertIn('updated_at', model_dict)
-
-if __name__ == '__main__':
-    unittest.main()
-
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
