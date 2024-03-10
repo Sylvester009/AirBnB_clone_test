d472b20ddf9ac5d15ddfeb318a55f035ac6ef69c
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-10 06:55:27 +0100

    Update console.py

--- console.py
+++ console.py
@@ -6,6 +6,11 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 class HBNBCommand(cmd.Cmd):
@@ -13,7 +18,8 @@ class HBNBCommand(cmd.Cmd):
     HBNBCommand console class
     """
     prompt = "(hbnb) "
-    valid_classes = ["BaseModel", "User"]
+    valid_classes = ["BaseModel", "User", "Amenity",
+                     "Place", "Review", "State", "City"]
 
     def emptyline(self):
         """
