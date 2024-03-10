9812e4b6c48e53f94553fd4daf368872a82e13d3
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-10 06:04:05 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,12 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
-        # If kwargs is not empty, set instance attributes accordingly
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                        setattr(self, key, datetime
+                                .strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                     else:
                         setattr(self, key, value)
 
@@ -31,13 +30,13 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime.
+        Updates updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        Returns a dict containing all keys/values of __dict__ of the instance.
         """
         obj_instance = self.__dict__.copy()
         obj_instance['__class__'] = self.__class__.__name__
